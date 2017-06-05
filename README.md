786ec636fedfbeec99dc683a9cc0a231599dc90c
sridixit <sriharidixit@gmail.com>
2017-06-05 00:46:24 -0400

    app folder created

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ members> npm install
 #Then execute below command to start the localhost
 
 members> npm start
+
+##Unit test case steps:
+
+ 1. npm install karma --save-dev
+ 2. npm install jasmine-core --save-dev
+ 3. npm install karma-jasmine --save-dev
+ 4. npm install karma-chrome-launcher --save-dev
+ 5. npm install -g karma-cli
--- app/controllers.js
+++ app/controllers.js
@@ -3,6 +3,13 @@ angular.module('myApp.controllers', [])
   .controller('MembersContrllers', ['$scope','mockData', function($scope, mockData) {
         $scope.members = mockData;
         // console.log($scope.members[0]);
+        var firstRecord = $scope.members[0]; 
+        $scope.fullName = firstRecord.firstName + " " + firstRecord.surname;
+        $scope.imgSrc = firstRecord.portrait;
+        $scope.motto = JSON.stringify(firstRecord.motto);
+        $scope.gender = firstRecord.gender;
+        $scope.birthday = firstRecord.birthday;
+        $scope.state = firstRecord.state;
         $scope.getMemberDetails = function(member) {
           console.log(member)
           $scope.fullName = member.firstName + " " + member.surname;
